ca98fb2fdb4d50c320cd312fc6c59cececf8eda3
Alex <alex.smotritsky@gmail.com>
2021-12-30 15:18:34 -0500

    fixed error display, formatting, field initialization

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import DynamicForm from './DynamicForm';
 
 function App() {
   const fields = [
-    {label: 'First Name', type: 'input', name: 'firstName', value: 'Abdi'},
-    {label: 'Last Name', type: 'input', name: 'lastName', value: 'Ahmed'},
-    {label: 'City', type: 'input', name: 'city', value: 'London'},
-    {label: 'Address', type: 'input', name: 'address', value: '10 FSS Street'},
+    {label: 'First Name', type: 'input', name: 'firstName', value: ''},
+    {label: 'Last Name', type: 'input', name: 'lastName', value: ''},
+    {label: 'City', type: 'input', name: 'city', value: ''},
+    {label: 'Address', type: 'input', name: 'address', value: ''},
     {label: 'Occupation', type: 'select', data: ['Teacher', 'Software Engineer', 'Doctor', 'Lawyer'], name: 'occupation', value: 'Please Select'},
     {label: 'Message', type: 'textarea', name: 'message', value: ''},
     {label: 'Agree to Terms & Conditions', type: 'checkbox', name: 'terms', value: false},
